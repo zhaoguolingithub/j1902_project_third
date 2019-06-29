2447eebf7b1c1d0462b8c12ee0533d6578beca8e
zhaoguolingithub <ezhaoguolin@163.com>
2019-06-29 11:50:52 +0800

    zhaoguolin-02

--- j1902_eureka_loginandreg/src/main/java/com/qf/j1902/J1902EurekaLoginandregApplication.java
+++ j1902_eureka_loginandreg/src/main/java/com/qf/j1902/J1902EurekaLoginandregApplication.java
@@ -2,8 +2,6 @@ package com.qf.j1902;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableDiscoveryClient
 //@EnableFeignClients
